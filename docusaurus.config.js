27ff0825ecb8da6f238c5183efbd900eb9339f7f
Benoît Rognier <benoit.rognier@edulera.com>
2021-07-15 10:06:25 +0200

    initial

---
+++ docusaurus.config.js
@@ -0,0 +1,114 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'My Site',
+  tagline: 'Dinosaurs are cool',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+  themeConfig: {
+    colorMode : {
+      defaultMode : 'dark',
+      disableSwitch: true,
+      respectPrefersColorScheme: false,
+    },
+    navbar: {
+      title: '',
+      logo: {
+        alt: 'Werenode portal',
+        src: 'img/werenode_logo_no_name_250px.png',
+      },
+      items: [
+        {
+          type: 'doc',
+          docId: 'intro',
+          position: 'left',
+          label: 'Tutorial',
+        },
+        {to: '/blog', label: 'Blog', position: 'left'},
+        {
+          href: 'https://github.com/facebook/docusaurus',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [
+        {
+          title: 'Docs',
+          items: [
+            {
+              label: 'Tutorial',
+              to: '/docs/intro',
+            },
+          ],
+        },
+        {
+          title: 'Community',
+          items: [
+            {
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+            },
+            {
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
+            },
+            {
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
+            },
+          ],
+        },
+        {
+          title: 'More',
+          items: [
+            {
+              label: 'Blog',
+              to: '/blog',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+    },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
---
+++ src/pages/index.js
@@ -0,0 +1,128 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import styles from './index.module.css';
+import { Grid, Typography } from '@material-ui/core';
+import useBaseUrl from '@docusaurus/useBaseUrl';
+//import Plug from '../../static/img/shutterstock_1868850010.jpeg';
+//import Plug from '../../static/img/shutterstock_1868849356.jpeg';
+import Plug from '../../static/img/shutterstock_1859956345.jpeg';
+import Map from '../../static/img/home_bg1.jpeg';
+
+const texts = {
+  tagline : 'This is the very important portals header tagline',
+  mobiletagline : 'This is the very important mobile app tagline'
+}
+
+const layoutOptions = {
+  right : {
+    angle : '225',
+    direction : 'row-reverse'
+  },
+  left : {
+    angle : '135',
+    direction : 'row'
+  }
+}
+
+const layout = 'left'
+
+const WerenodeHeader = () => {
+  return (
+    <Grid container direction={layoutOptions[layout].direction} justifyContent="flex-start" alignItems="flex-start" style={{
+      height: "500px",
+      backgroundImage : `linear-gradient(${layoutOptions[layout].angle}deg, rgb(0 76 126 / 100%), rgb(0 76 126 / 90%), rgb(0 76 126 / 0%)), url(${Plug})`,
+      backgroundSize : 'cover'
+    }}>
+      <Grid container direction="row" justifyContent="center" alignItems="center" item md={6} sm={12} xs={12}>
+        <Grid item xs={12} style={{
+            margin    : '50px',
+            textAlign : 'center'
+          }}>
+          <img src={useBaseUrl('img/werenode_logo_text_250px.png')} style={{ width : '350px' }}/>
+        </Grid>
+        <Grid item style={{
+          margin      : '50px',
+          textAlign   : 'center'
+         }}>
+          <Typography variant='h3' xs={12}>{texts.tagline}</Typography>
+        </Grid>
+      </Grid>
+    </Grid>
+  )
+}
+
+const Phone = () => {
+  return (
+    <div style={{
+      position : 'relative',
+      width    : '500px',
+      height   : '650px',
+      overflow : 'hidden'
+    }}>
+      <img src={useBaseUrl('img/app_screen1.jpg')} style={{
+        position : 'absolute',
+        top: '64px',
+        left: '79px',
+        width: '342px',
+      }}/>
+      <img src={useBaseUrl('img/phone.png')} style={{ width : '500px', position : 'absolute', top : '0px', left : '0px' }}/>
+    </div>
+  )
+}
+
+const GooglePlayStore = () => {
+  return (
+    <img src={useBaseUrl('img/google-play-badge.png')} style={{ width : '200px' }}/>
+  )
+}
+
+const AppleStore = () => {
+  return (
+    <img src={useBaseUrl('img/AppStoreButton.png')} style={{ width : '150px' }}/>
+  )
+}
+
+const MobileApp = () => {
+  return (
+    <Grid container direction="row" justifyContent="flex-start" alignItems="flex-start" style={{
+      height: "650px",
+      backgroundImage : `url(${Map})`,
+      backgroundSize : 'cover'
+    }}>
+    <Grid container direction="row" justifyContent="center" alignItems="center" item md={6} sm={12} xs={12}>
+      <Grid item><Phone /></Grid>
+    </Grid>
+    <Grid container direction="column" justifyContent="space-between" alignItems="center" style={{ height: '100%' }} item md={4} sm={12} xs={12} >
+      <Grid item style={{
+          marginTop   : '100px',
+          textAlign   : 'center'
+         }}>
+          <Typography variant='h3'>{texts.mobiletagline}</Typography>
+      </Grid>
+      <Grid container direction="row" justifyContent="center" alignItems="center" spacing={4} item>
+        <Grid item>
+          <GooglePlayStore />
+        </Grid>
+        <Grid item>
+          <AppleStore />
+        </Grid>
+      </Grid>
+    </Grid>
+    </Grid>
+  )
+}
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title="Werenode Portal"
+      description="Description will go into a meta tag in <head />">
+      <WerenodeHeader />
+      <MobileApp />
+    </Layout>
+  );
+}
