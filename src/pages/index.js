2bb44b8bc116b436ad49769818ac9f0a57023edb
Benoît Maïsseu <54311479+bmaisseu@users.noreply.github.com>
2022-10-14 13:16:30 +0200

    Switch places FrenchTech - Ontochain

--- src/pages/index.js
+++ src/pages/index.js
@@ -157,15 +157,15 @@ const data = {
       width: '250px',
     },
     {
-      key: 'FrenchTech',
-      img: 'img/lafrenchtechsaclay.png',
-      url: 'https://lafrenchtech-paris-saclay.fr',
-      width: '200px',
-    },
-    {
       key: 'Ontochain',
       img: 'img/ontochain.png',
       url: 'https://ontochain.ngi.eu',
+      width: '200px',
+    },
+    {
+      key: 'FrenchTech',
+      img: 'img/lafrenchtechsaclay.png',
+      url: 'https://lafrenchtech-paris-saclay.fr',
       width: '200px',
     },
   ]
